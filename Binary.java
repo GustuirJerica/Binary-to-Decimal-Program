103672aaa7edb34a1719a3111b999ee7dc9c11c4
GustuirJerica <100286596+GustuirJerica@users.noreply.github.com>
2022-03-30 16:19:01 +0800

    Second commit

--- Binary.java
+++ Binary.java
@@ -14,7 +14,7 @@
  ******************************************************************/
  import javax.swing.*;
 
-public class BinaryProgram {
+public class Binary {
     public static void main(String[] args)
       {
         
